f12bc1570026122b353e21982d934a1214c6c3a2
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2023-12-25 22:40:03 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,7 +1,3 @@
 function solution(phone_number) {
-    let answer = '';
-    for (let [index, num] of [...phone_number].entries()) {
-        answer += index >= phone_number.length - 4 ? num : "*";
-    }
-    return answer;
+    return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
 }
