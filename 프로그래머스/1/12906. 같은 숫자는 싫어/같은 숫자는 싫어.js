aee591f20f021b77e4fb7591612e93d3fbaeafd5
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2023-12-27 23:05:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.04 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,3 @@
 function solution(arr) {
-    let prevNum = Number.MIN_SAFE_INTEGER;
-    return arr.reduce((acc, cur) => {
-        if (cur !== prevNum) {
-            acc.push(cur);
-            prevNum = cur;
-        }
-        return acc;
-    }, []);
+    return arr.filter((val, idx) => val !== arr[idx + 1]);
 }
