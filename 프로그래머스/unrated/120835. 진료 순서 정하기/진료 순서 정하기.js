55171a25c1c4d67f28c06eed9aa114728dd8506a
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2023-12-29 02:27:03 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,11 +1,4 @@
 function solution(emergency) {
-    const length = emergency.length;
-    const answer = Array.from({length: length}, () => 1);
-    
-    for (let i = 1; i < length; i++) {
-        for (let j = i - 1; j >= 0; j--) {
-            emergency[j] < emergency[i] ? answer[j]++ : answer[i]++;
-        }
-    }
-    return answer;
+    const sorted = [...emergency].sort((a, b) => b - a);
+    return emergency.map((v) => sorted.indexOf(v) + 1);
 }
