584f2168a35f313b925ca44775045b6bdcfea189
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2023-12-29 02:35:47 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,12 @@
+
+
+function solution(n) {
+    const answer = [];
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+       if (n % i === 0) {
+            answer.push(i);
+            if (n / i !== i) answer.push(n / i);
+        }
+    }
+    return answer.length;
+}
