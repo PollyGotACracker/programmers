d0ea18646b187603daea4c7c1a113f1ea1698abc
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2023-12-29 01:18:29 +0900

    [level unrated] Title: 배열 만들기 4, Time: 4.86 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    const stk = [];
+    let i = 0;
+    
+    while (i < arr.length) {
+        if (!stk.length || stk.at(-1) < arr[i]) {
+            stk.push(arr[i]);
+            i++;         
+        }
+        else if (stk.at(-1) >= arr[i]) stk.pop();     
+    }
+    return stk;
+}
