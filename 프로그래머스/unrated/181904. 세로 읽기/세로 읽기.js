98108036627ce30ab3fc5895ea49833bcea08de4
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2023-12-31 00:52:33 +0900

    [level unrated] Title: 세로 읽기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,8 +1,3 @@
 function solution(my_string, m, c) {
-    let answer = '';
-    
-    for (let i = 0; i <= my_string.length - m; i += m) {
-        answer += my_string[i + c - 1];
-    }
-    return answer;
+    return [...my_string].filter((_, i) => i % m === c - 1).join("");
 }
