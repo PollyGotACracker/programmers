640f506ca16fc6a9e299eaa2ad0768ea573a222e
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2023-12-31 01:33:43 +0900

    [level unrated] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -1,6 +1,3 @@
-function solution(hp, ants = 0) {
-    if (hp === 0) return ants;
-    if (hp < 3) return ants + hp;
-    if (hp < 5) return solution(hp % 3, ants + Math.floor(hp / 3));
-    return solution(hp % 5, ants + Math.floor(hp / 5));
+function solution(hp) {
+    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + ((hp % 5) % 3);
 }
