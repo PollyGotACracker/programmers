1e1b3ceae90f4f8d6fa7130905704ee985e2de19
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-01 15:36:35 +0900

    [level unrated] Title: 2의 영역, Time: 0.36 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
@@ -1,16 +1,6 @@
 function solution(arr) {
-    let flag = false;
-    let s = Number.MIN_SAFE_INTEGER;
-    let e = Number.MIN_SAFE_INTEGER;
+    const s = arr.indexOf(2);
+    const e = arr.lastIndexOf(2);
     
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== 2) continue;
-        if (!flag) {
-            s = i;
-            e = i;
-            flag = true;
-        }
-        else e = i;
-    }
-    return s >= 0 ? arr.slice(s, e + 1) : [-1];
+    return s !== -1 ? arr.slice(s, e + 1) : [-1];
 }
