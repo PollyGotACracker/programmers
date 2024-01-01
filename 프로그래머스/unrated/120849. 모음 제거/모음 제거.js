66caeaab884134087bb8a5e7ecf7b80a66757f6d
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-01 17:35:55 +0900

    [level unrated] Title: 모음 제거, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
@@ -1,5 +1,3 @@
 function solution(my_string) {
-    const removes = ["a", "e", "i", "o", "u"];
-    const arr = [...my_string].filter((v) => !removes.includes(v));
-    return arr.join("");
+    return my_string.replace(/[aeiou]/g, "");
 }
