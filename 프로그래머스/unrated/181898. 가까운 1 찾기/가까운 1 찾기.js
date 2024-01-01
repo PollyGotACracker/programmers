04b2f1bc3d6899cae9b65f23b9047b4769282d37
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-01 14:41:11 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,6 +1,4 @@
 function solution(arr, idx) {
-    for (let i = idx; i < arr.length; i++) {
-        if (arr[i] === 1) return i;
-    }
-    return -1;
+    // indexOf() 의 두 번째 매개변수는 시작 인덱스
+    return arr.indexOf(1, idx);
 }
