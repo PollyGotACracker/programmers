dfb5563e00f8526233f929262ee30f4530341fed
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-01 23:18:47 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.72 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,8 +1,5 @@
 function solution(A,B){
-    let answer = 0;
     A.sort((a, b) => a - b);
     B.sort((a, b) => b - a);
-    
-    A.forEach((v, i) => answer += v * B[i]);
-    return answer;
+    return A.reduce((acc, cur, idx) => acc += cur * B[idx], 0);
 }
