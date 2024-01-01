360e3c2126376137dfe0e314867a89fb91b0889c
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-01 23:11:15 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,4 +1,16 @@
 function solution(s) {
-    const toLower = s.toLowerCase();
-    return toLower.replaceAll(/(^|\s)[a-z]/g, (match) => match.toUpperCase());
+  const toLower = s.toLowerCase();
+  return toLower.replaceAll(/(^|\s)[a-z]/g, (match) => match.toUpperCase());
 }
+
+/*
+cf) replace(), replaceAll() 두 번째 매개변수는 콜백함수를 받는다.
+function replacer(match, p1, p2, …, pN, offset, string, groups) {
+  return replacement;
+}
+match: 일치하는 문자열
+p1, p2, …, pN: 정규표현식에서 캡처 그룹 () 으로 찾은 각 문자열
+offset: 검사한 전체 문자열에서 match 문자열의 인덱스
+string: 검사한 전체 문자열
+groups: 이름이 있는 캡처 그룹 (?<name>...) 객체: groups.name
+*/
