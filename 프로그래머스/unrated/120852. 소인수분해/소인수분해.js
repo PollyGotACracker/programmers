f8550f0014a278ce47e17460b9168730ba3488c9
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-01 18:27:30 +0900

    [level unrated] Title: 소인수분해, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.js
@@ -1,15 +1,12 @@
 function solution(n) {
     const answer = [];
     let divisor = 2;
-    let share = n;
     
-    while (share > 1) {
-        if (share % divisor !== 0) {
-            divisor++;
-            continue;
-        }
-        share = Math.floor(share / divisor);
-        if (answer.at(-1) !== divisor) answer.push(divisor);
+    while (n > 1) {
+        if (n % divisor === 0) {
+            n = Math.floor(n / divisor);
+            if (answer.at(-1) !== divisor) answer.push(divisor);
+        } else divisor++;
     } 
     return answer;
 }
