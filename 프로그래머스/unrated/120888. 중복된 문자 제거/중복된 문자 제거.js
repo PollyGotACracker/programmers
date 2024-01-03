5b6882d4367a7167607becd46d3c20fc2326a5c7
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-04 00:15:33 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,11 +1,4 @@
 function solution(my_string) {
-    const str = [];
-    const arr = Array.from(my_string).map((v) => {
-        if (str.includes(v)) return "";
-        else {
-            str.push(v);
-            return v;
-        }
-    });
-    return arr.join("");
+    const removed = new Set(my_string);
+    return Array.from(removed).join("");
 }
