f1551b14054da594148d657c6fe790cac83d7300
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-04 00:40:20 +0900

    [level unrated] Title: 가까운 수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/unrated/120890. 가까운 수/가까운 수.js
@@ -1,7 +1,5 @@
 function solution(array, n) {
-    array.sort((a, b) => a - b);
-    const subtracts = array.map((v) => Math.abs(v - n));
-    const closed = Math.min(...subtracts);
-    const index = subtracts.indexOf(closed);
-    return array[index];
+    // n - v 의 절대값 비교, v 직접 비교 ASC
+    array.sort((a, b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
+    return array[0];
 }
