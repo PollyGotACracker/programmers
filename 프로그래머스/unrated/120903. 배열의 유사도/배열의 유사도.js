00bfeb5fd3ce02aa074965d706a0403a0179ddb4
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-05 02:39:36 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
@@ -1,18 +1,6 @@
 function solution (arr1, arr2) {
-    let count = 0;
-    let i = 0;
-    let j = 0;
+    const concat = [...arr1, ...arr2];
+    const dedupe = Array.from(new Set(concat));
     
-    arr1.sort();
-    arr2.sort();
-    while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) i++;
-        else if (arr1[i] > arr2[j]) j++;
-        else {
-            count++;
-            i++;
-            j++;
-        }
-    }
-    return count;
+    return concat.length - dedupe.length;
 }
