34989c83a40ff0fb692448410fbfaca1930c9774
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-08 22:49:51 +0900

    [level unrated] Title: 잘라서 배열로 저장하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,8 +1,7 @@
 function solution(my_str, n) {
-    const answer = [];
-    
-    for (let i = 0; i < my_str.length; i += n) {
-        answer.push(my_str.substring(i, i + n));
-    }
-    return answer;
+    // cf) 문자열을 동일한 크기로 나누기
+    // 모든 문자에 대해 최소 1 개, 최대 n 개 일치
+    // 만약 n 개 미만의 부분문자열이 남더라도 일치함
+    const regex = new RegExp(`.{1,${n}}`, "g");
+    return my_str.match(regex);
 }
