cbeb1671473676ad2b6ba769f0aa7d4704286b63
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-08 20:10:22 +0900

    [level unrated] Title: 세 개의 구분자, Time: 9.68 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,5 +1,6 @@
 function solution(myStr) {
-    const regex = new RegExp(/[abc]{1,}/, "g");
-    const arr = myStr.split(regex).filter((v) => v);
-    return arr.length ? arr : ["EMPTY"];
+    // 문자열 myStr 을 "a", "b", "c" 기준으로 나누기
+    // 1. match(): 기준과 일치하지 않는(^) 부분 문자열을 찾는다
+    // 2. split(): 일치하는 부분을 기준으로 문자열을 나눈다(배열에 "" 도 포함될 수 있음)
+    return myStr.match(/[^a-c]+/g)||['EMPTY'];
 }
