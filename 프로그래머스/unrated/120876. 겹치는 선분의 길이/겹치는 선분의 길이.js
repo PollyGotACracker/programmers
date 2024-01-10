731da70a68f5b266d4206a9ffae77a9afdc992f8
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-11 04:03:39 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,24 +1,10 @@
-function getOverlapped([s1, e1], [s2, e2]) {
-    const start = Math.max(s1, s2);
-    const end = Math.min(e1, e2);
-    if (end > start) return [start, end];
-}
-
 function solution(lines) {
-    const overlaps = [];
-    for (let i = 0; i < lines.length; i++) {
-        for (let j = i + 1; j < lines.length; j++) {
-            const section = getOverlapped(lines[i], lines[j]);
-            if (section) overlaps.push(section);
-        }
-    }
-    overlaps.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+    // -100 ~ 100 구간 => 0 ~ 199
+    const axis = new Array(200).fill(0);
     
-    let totalLength = overlaps.reduce((acc, [start, end]) => acc + Math.abs(end - start), 0);
-    for (let i = 0; i < overlaps.length - 1; i++) {
-        const curEnd = overlaps[i][1];
-        const nextStart = overlaps[i + 1][0];
-        if (nextStart < curEnd) totalLength -= curEnd - nextStart;
-    }
-    return totalLength;
+    // 초기화문 생략. start ~ end - 1 동안 start + 100 위치의 값 증가 후 start++
+    lines.forEach(([start, end]) => { 
+        for(; start < end; start++) axis[start + 100]++; 
+    });
+    return axis.reduce((acc, cur) => cur > 1 ? acc + 1 : acc, 0);
 }
