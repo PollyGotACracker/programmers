5aee6ce284e50e091a8e40cd6fb957fd6180e616
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-11 00:27:29 +0900

    [level unrated] Title: 평행, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120875. 평행/평행.js
+++ 프로그래머스/unrated/120875. 평행/평행.js
@@ -1,23 +1,13 @@
-function solution(dots) {
-    let answer = 0;
-    const length = dots.length;
-    
-    for (let i = 0; i < dots.length; i++) {
-        for (let j = i + 1; j < dots.length; j++) {
-            if (i === length - 1 - j || j === length - 1 - i) continue;
-            const [x1, y1] = dots[i];
-            const [x2, y2] = dots[j];
-            const [x3, y3] = dots[length - 1 - i];
-            const [x4, y4] = dots[length - 1 - j];
-            
-            const m1 = (y2 - y1) / (x2 - x1);
-            const m2 = (y4 - y3) / (x4 - x3);
-            
-            if (m1 === m2) {
-                answer = 1;
-                break;
-            }
-        }
-    }
-    return answer;
+function calcSlope([x1, y1], [x2, y2]) {
+    return (y2 - y1) / (x2 - x1);
 }
+
+function solution(dots) {
+    const [arr1, arr2, arr3, arr4] = dots; 
+    
+    if (calcSlope(arr1, arr2) === calcSlope(arr3, arr4)) return 1;
+    if (calcSlope(arr1, arr3) === calcSlope(arr2, arr4)) return 1;
+    if (calcSlope(arr1, arr4) === calcSlope(arr2, arr3)) return 1;
+    return 0;
+}
+
