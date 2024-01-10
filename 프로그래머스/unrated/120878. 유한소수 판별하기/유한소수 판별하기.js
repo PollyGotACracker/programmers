4f0b7324e50edb47fb681aae05812ddb03ab8599
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-10 21:35:39 +0900

    [level unrated] Title: 유한소수 판별하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -2,23 +2,12 @@ function getGcd(n1, n2) {
     return n2 === 0 ? n1 : getGcd(n2, n1 % n2);
 }
 
-function isPrime(n) {
-    const sqrt = Math.floor(Math.sqrt(n));
-    let i = 2;
-    while (i <= sqrt) {
-        if (n % i === 0) return false;
-        i++;
-    }
-    return true;
-}
-
 function solution(a, b) {
     const gcd = getGcd(a, b);
-    const dividedB = b / gcd;
-    let i = 2;
-    while (i <= dividedB) {
-        if (isPrime(i) && i !== 2 && i !== 5 && dividedB % i === 0) return 2;
-        i++;
-    }
-    return 1;
+    b /= gcd;
+    
+    while (b % 2 === 0) b /= 2;
+    while (b % 5 === 0) b /= 5;
+    
+    return b === 1 ? 1 : 2;
 }
