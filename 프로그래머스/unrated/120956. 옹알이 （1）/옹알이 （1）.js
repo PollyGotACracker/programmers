1bed39f8e4bf5f3faa9788382485a35ce0395f77
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-10 22:26:31 +0900

    [level unrated] Title: 옹알이 (1), Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,9 +1,7 @@
 function solution(babbling) {
     let answer = 0;
-    const regex = new RegExp(/\b(aya|ye|woo|ma)+\b/);  
+    const regex = new RegExp(/^(aya|ye|woo|ma)+$/);  
     
-    for (let v of babbling) {
-        if (regex.test(v)) answer++;
-    }
+    babbling.forEach((v) => {if (regex.test(v)) answer++});
     return answer;
 }
