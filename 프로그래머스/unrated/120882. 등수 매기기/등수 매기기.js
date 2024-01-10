c7ac250dcf0b3e4d24cf345a018a93934d62fdf1
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-10 22:20:37 +0900

    [level unrated] Title: 등수 매기기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
@@ -1,5 +1,6 @@
 function solution(score) {
-    const avgs = score.map(([eng, math]) => (eng + math) / 2);
-    const sortedAvgs = [...avgs].sort((a, b) => b - a);
-    return avgs.map((v) => sortedAvgs.indexOf(v) + 1);
+    // 현재 학생의 평균보다 높은 평균을 가진 학생들의 수 + 1
+    return score.map(([eng1, math1]) => {
+        return score.filter(([eng2, math2]) => (eng2 + math2) / 2 > (eng1 + math1) / 2).length + 1;
+    });
 }
