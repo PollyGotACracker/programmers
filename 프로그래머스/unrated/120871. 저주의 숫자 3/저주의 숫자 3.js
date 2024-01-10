4a10d086e1070d157b20a26fbd0b25afb20c9326
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-11 00:19:03 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,15 +1,6 @@
 function solution(n) {
-    let i = 1;
-    let diff = 0;
-
-    while (i <= n) {
-        const value = i + diff;
-        const isIncluded = value.toString().includes(3);
-        if (isIncluded || value % 3 === 0) {
-            diff++;
-            continue;
-        }
-        i++;
-    }
-    return n + diff;
+    // length: n * 3: 조건을 고려하여 충분한 크기의 배열 생성
+    // [n - 1]: 3을 포함하지 않으면서 3의 배수가 아닌 숫자 배열에서 n 번째 숫자를 찾음(0 ~ 1)
+    const arr = Array.from({length: n * 3}, (_, i) => i + 1);
+    return arr.filter((num) => num % 3 !== 0 && !num.toString().includes("3"))[n - 1];
 }
