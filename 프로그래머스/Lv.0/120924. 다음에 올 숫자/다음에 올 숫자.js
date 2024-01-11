8f39c5d66ffd5ef0f8c332313913aca561aab80d
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-12 02:50:18 +0900

    [level Lv.0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/Lv.0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,9 +1,8 @@
 function solution(common) {
     const [v0, v1, v2] = common;
-    const vLast = common.at(-1);
     
-    const isArith = v1 - v0 === v2 - v1;
-    const diff = isArith ? v1 - v0 : v1 / v0;
-    
-    return isArith ? vLast + diff : vLast * diff;
+    if (v1 - v0 === v2 - v1) {
+        return common.pop() + (v1 - v0);
+    }
+    else return common.pop() * (v1 / v0);
 }
