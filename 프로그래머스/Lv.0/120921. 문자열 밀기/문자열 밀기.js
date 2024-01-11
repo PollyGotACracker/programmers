76836da5258bfe7035c82166e2d1ee2b5cf3ae89
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-12 01:36:01 +0900

    [level Lv.0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/Lv.0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,13 +1,4 @@
 function solution(A, B) {
-    const arr = [...A];
-    let i = 1;
-    
-    if (A === B) return 0;
-    while (i < A.length) {
-        const char = arr.pop();
-        arr.unshift(char);
-        if (arr.join("") === B) return i;
-        i++;
-    }
-    return -1;
+    // 2개의 B를 합친 후 A 찾기
+    return (B + B).indexOf(A);
 }
