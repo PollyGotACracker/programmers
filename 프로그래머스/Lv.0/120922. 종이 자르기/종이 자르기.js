9796fabcc3b9d869e35b747d6a66bd869659d0b6
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-12 01:45:31 +0900

    [level Lv.0] Title: 종이 자르기, Time: 0.02 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120922. 종이 자르기/종이 자르기.js
+++ 프로그래머스/Lv.0/120922. 종이 자르기/종이 자르기.js
@@ -1,5 +1,6 @@
 function solution(M, N) {
-    const sliceM = M - 1;
-    const sliceN = M * (N - 1);
-    return sliceM + sliceN;
+    // const sliceM = M - 1;
+    // const sliceN = M * (N - 1);
+    // return sliceM + sliceN;
+    return M * N - 1;
 }
