66e92ce2ac113d05d5a9bbb8b71f2fbdfeecbaf4
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-01-16 01:10:02 +0900

    [level Lv.1] Title: 로또의 최고 순위와 최저 순위, Time: 0.10 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/Lv.1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/Lv.1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -11,3 +11,31 @@ function solution(lottos, win_nums) {
     }
     return answer.map((v) => v === 7 ? 6 : v);
 }
+
+/* cf) includes() 를 쓰는 것보다 Set 을 생성하고 has() 를 쓰는 것이 더 효율적이다(O(1)). */
+/*
+일부 테스트 케이스에서 시간 초과가 발생한 코드
+function solution(lottos, win_nums) {
+    const answer = [7, 7];
+    let i = 0;
+    let j = 0;
+    
+    lottos.sort((a, b) => a - b);
+    win_nums.sort((a, b) => a - b);
+    while (i < lottos.length) {
+        if (lottos[i] === 0) {
+            answer[0]--;
+            i++;
+        }
+        else if (lottos[i] === win_nums[j]){
+            answer[0]--;
+            answer[1]--;
+            i++;
+            j++;
+        }
+        else if (lottos[i] < win_nums[j]) i++;
+        else if (lottos[i] > win_nums[j]) j++;
+    }
+    return answer.map((v) => v === 7 ? 6 : v);
+}
+*/
