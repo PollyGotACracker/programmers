6e7ef3e0d727ebbe8fb9ea150054041a97a04a8b
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-03-26 01:52:28 +0900

    [level 1] Title: 둘만의 암호, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,13 +1,16 @@
 function solution(s, skip, index) {
-    const skips = new Set(skip);
-    return s.split('').reduce((acc, cur) => {
-        let i = 0;
-        while (i < index) {
-            let code = cur.charCodeAt(0);
-            code = code >= 122 ? 97 : code + 1;
-            cur = String.fromCharCode(code);
-            if (!skips.has(cur)) i++;
-        }
-        return acc += cur;
-    }, "");
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", 
+                      "h", "i", "j", "k", "l", "m", "n", 
+                      "o", "p", "q", "r", "s", "t", "u", 
+                      "v", "w", "x", "y", "z"].filter(char => !skip.includes(char));
+    return s.split("").map(char => {
+        return alphabet[(alphabet.indexOf(char) + index) % alphabet.length]
+    }).join("");
 }
+
+/*
+1. skip 을 제외한 alphabet 배열을 생성
+2. alphabet 배열에서 s 의 각 문자의 인덱스를 찾은 후, index 만큼 뒤로 이동
+3. 위 결과값이 alphabet.length 를 넘어갈 수 있음 (z 를 넘어갈 수 있음)
+4. 그러므로 % alphabet.length 한 나머지를 인덱스로 함
+*/
