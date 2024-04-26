13138a88a8a19cb9f0e1cd877c5eabfb4b797de3
PollyGotACracker <92136750+PollyGotACracker@users.noreply.github.com>
2024-04-26 21:53:44 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.54 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,40 +1,17 @@
 function solution(survey, choices) {
-    let answer = '';
-    const types = {
-        "R": 0,
-        "T": 0,
-        "C": 0,
-        "F": 0,
-        "J": 0,
-        "M": 0,
-        "A": 0,
-        "N": 0,
-    };
-    const choiceScores = {
-        1: 3,
-        2: 2,
-        3: 1,
-        4: 0,
-        5: 1,
-        6: 2,
-        7: 3,
-    };
+    const types = ["RT", "CF", "JM", "AN"];
+    const scores = {};
     
-    for (let i = 0; i < survey.length; i++) {
-        const [typeA, typeB] = survey[i].split("");
-        if (choices[i] <= 3) {
-            types[typeA] += choiceScores[choices[i]];
-        }
-        if (choices[i] >= 5) {
-            types[typeB] += choiceScores[choices[i]];
-        }
-    }
+    // 문자열 split 대신 구조분해할당
+    types.forEach(([a, b]) => {
+        scores[a] = 0;
+        scores[b] = 0;
+    });
     
-    answer = Object.entries(types).reduce((acc, cur, idx, arr) => {
-        if (idx % 2 === 1) return acc;
-        const nextType = arr[idx + 1];
-        return `${acc}${cur[1] >= nextType[1] ? cur[0] : nextType[0]}`
-    }, "");
-    
-    return answer;
+    choices.forEach((choice, index) => {
+        const [typeA, typeB] = survey[index];
+        scores[choice < 4 ? typeA : typeB] += Math.abs(choice - 4);  
+    }); 
+
+    return types.map(([a, b]) => scores[a] >= scores[b] ? a : b).join("");
 }
